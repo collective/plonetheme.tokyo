5e5d4f013e80dd1ecfa60218a1890c9a1ec31970
Stefan Arnold <stefan.arnold@operun.de>
2019-05-21 14:13:31 +0200

    Make IPlonethemeTokyoLayer inherit ICollectiveSidebarLayer for jbot overrides.

--- src/plonetheme/tokyo/interfaces.py
+++ src/plonetheme/tokyo/interfaces.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
+from collective.sidebar.interfaces import ICollectiveSidebarLayer
 
-from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
-
-class IPlonethemeTokyoLayer(IDefaultBrowserLayer):
+class IPlonethemeTokyoLayer(ICollectiveSidebarLayer):
     """Marker interface that defines a browser layer."""
