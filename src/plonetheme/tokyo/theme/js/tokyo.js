51abbb6d2ca74761b2a55b1830a8992f0fc0fffb
Jesse Stippel <jesse.stippel@operun.de>
2019-06-28 22:58:14 +0200

    fix js

--- src/plonetheme/tokyo/theme/js/tokyo.js
+++ src/plonetheme/tokyo/theme/js/tokyo.js
@@ -1,29 +1,5 @@
-if (window.jQuery) {
-  define('jquery', [], function() {
-    'use strict';
-    return window.jQuery;
-  });
-}
+$(document).ready(function() {
 
-require([
-  'mockup-patterns-relateditems',
-  'mockup-patterns-querystring',
-  'mockup-patterns-tinymce',
-  'mockup-patterns-inlinevalidation',
-  'mockup-patterns-structure',
-  'mockup-patterns-structureupdater',
-  'mockup-patterns-recurrence',
-], function() {
-  'use strict';
+  // Custom JavaScript
+
 });
-
-(function($) {
-  'use strict'; // Start of use strict
-
-  $(document).ready(function() {
-
-    // Custom JavaScript
-
-  });
-
-})(jQuery); // End of use strict
