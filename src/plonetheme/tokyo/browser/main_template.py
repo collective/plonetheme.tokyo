1e905eef0ff36a59587bbf643046b8fc38971279
Jesse Stippel <jesse.stippel@operun.de>
2019-07-18 15:07:35 +0200

    Plone 520 (#18)
    * Update package to Plone 5.2

---
+++ src/plonetheme/tokyo/browser/main_template.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from Products.CMFPlone.browser.interfaces import IMainTemplate
+from Products.Five import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.interface import implementer
+
+
+@implementer(IMainTemplate)
+class MainTemplate(BrowserView):
+
+    ajax_template = ViewPageTemplateFile('templates/ajax_main_template.pt')
+    main_template = ViewPageTemplateFile('templates/main_template.pt')
+
+    def __call__(self):
+        return self.template()
+
+    @property
+    def template(self):
+        if self.request.form.get('ajax_load'):
+            return self.ajax_template
+        else:
+            return self.main_template
+
+    @property
+    def macros(self):
+        return self.template.macros
