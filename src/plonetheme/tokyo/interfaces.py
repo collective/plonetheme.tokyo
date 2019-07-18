e7dc78ab14355d8004859ce1e4386f8ce6b00285
Jesse Stippel <jesse.stippel@operun.de>
2019-07-18 14:19:40 +0200

    main_template fix

--- src/plonetheme/tokyo/interfaces.py
+++ src/plonetheme/tokyo/interfaces.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
-from collective.sidebar.interfaces import ICollectiveSidebarLayer
+from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
 
-class IPlonethemeTokyoLayer(ICollectiveSidebarLayer):
+class IPlonethemeTokyoLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
