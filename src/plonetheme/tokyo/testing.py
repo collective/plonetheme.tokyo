d4599e78e45f28369d3a3db1be447b330cffe4ef
ale-rt <alessandro.pisa@gmail.com>
2019-10-26 14:22:27 +0200

    Restore barceloneta after the theme has been uninstalled
    Refs. #32

--- src/plonetheme/tokyo/testing.py
+++ src/plonetheme/tokyo/testing.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-
 from Acquisition import aq_get
+from plone import api
 from plone.app.contenttypes.testing import PLONE_APP_CONTENTTYPES_FIXTURE
 from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
-from plone.app.testing import applyProfile
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import PloneSandboxLayer
@@ -72,7 +71,8 @@ class PlonethemeTokyoLayer(PloneSandboxLayer):
         self.loadZCML(package=collective.sidebar)
 
     def setUpPloneSite(self, portal):
-        applyProfile(portal, 'plonetheme.tokyo:default')
+        qi = api.portal.get_tool('portal_quickinstaller')
+        qi.installProduct('plonetheme.tokyo')
         portal.acl_users.userFolderAddUser(
             SITE_OWNER_NAME,
             SITE_OWNER_PASSWORD,
