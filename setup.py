51d2d67e051e2bf94a69f7710d45e7e9fb5e45ee
Stefan Antonelli <stefan.antonelli@operun.de>
2020-10-06 23:26:49 +0200

    Back to development: 0.6.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='plonetheme.tokyo',
-    version='0.5.0',
+    version='0.6.0.dev0',
     description="An add-on for Plone",
     long_description=long_description,
     classifiers=[
