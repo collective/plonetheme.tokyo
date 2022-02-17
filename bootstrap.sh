18baf46de0b0f7763293f6761ec9eb918b0f2731
Stefan Antonelli <stefan.antonelli@operun.de>
2022-02-17 16:13:17 +0100

    testing

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,4 @@
-[ ! -f bin/pip ] && virtualenv .
-bin/pip install --upgrade pip setuptools zc.buildout
-bin/buildout -c $1 annotate | tee annotate.txt | grep -E 'setuptools *= *[0-9][^ ]*|zc.buildout *= *[0-9][^ ]*'| sed 's/= /==/' > requirements.txt
-cat annotate.txt
-cat requirements.txt
-bin/pip install --upgrade -r requirements.txt
+#/bin/sh
+`which python3.7` -m venv .
+./bin/pip install -r requirements.txt
+./bin/buildout -c develop.cfg
