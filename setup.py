77f5c22a6828ce57138a0d088b866a9c861cdb69
Stefan Antonelli <stefan.antonelli@operun.de>
2023-02-07 14:05:43 +0100

    fix build branch

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
