004758872710ed2c22a689b98959fef0569e38c2
Stefan Antonelli <stefan.antonelli@operun.de>
2023-02-07 14:08:59 +0100

    Preparing release 1.0.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='plonetheme.tokyo',
-    version='1.0.0.dev0',
+    version='1.0.0',
     description="An add-on for Plone",
     long_description=long_description,
     classifiers=[
