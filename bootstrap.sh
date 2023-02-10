6a4f88e4f998d14b07b3c390ab269f52545a367d
Lena Daxenbichler <lena.daxenbichler@r2solutions.at>
2023-02-10 15:22:05 +0100

    update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
 #/bin/sh
-`which python3` -m venv .
-./bin/pip install -r requirements.txt
+`which python3` -m venv venv
+./venv/bin/pip install -r requirements.txt
+./venv/bin/buildout bootstrap
 ./bin/buildout -c develop.cfg
